2e1feb25025b1c4cd20cacc2a4cdfefca245c8e3
nacosof <nacosof@gmail.com>
2026-04-28 19:49:58 +0500

    prisma migrations

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "prisma/config";
 import path from "node:path";
+import "dotenv/config";
 
 export default defineConfig({
   schema: path.join("prisma", "schema.prisma"),

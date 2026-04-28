10c31a797762981a2ee3ffb5487fae84b43d402e
nacosof <nacosof@gmail.com>
2026-04-28 19:20:34 +0500

    auto en language

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -1,4 +1,4 @@
-export const locales = ["ru", "en"] as const;
+export const locales = ["en", "ru"] as const;
 export type Locale = (typeof locales)[number];
 
 export const defaultLocale: Locale = "en";

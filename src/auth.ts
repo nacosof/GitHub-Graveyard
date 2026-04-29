fafa9a933692b0bb0eff88ea0af5b7214b09f1a6
nacosof <nacosof@gmail.com>
2026-04-29 11:44:49 +0500

    .

--- src/auth.ts
+++ src/auth.ts
@@ -61,6 +61,7 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID ?? "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+      allowDangerousEmailAccountLinking: true,
     }),
     CredentialsProvider({
       name: "Credentials",
